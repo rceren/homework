b6ee4b652a0e407c6cce6b10fb70171b5bdfefb1
rceren <144117480+rceren@users.noreply.github.com>
2023-10-28 20:05:39 +0300

    Add files via upload

---
+++ pt1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+ {
+    long long num1 = 400000LL;
+    long long num2 = 100000LL;
+    long long result;
+
+    // perform the multiplication
+    result = num1 * num2;
+
+    // output of the result
+    printf("The result of the multiplication is: %lld\n", result);
+
+    return 0;
+}
